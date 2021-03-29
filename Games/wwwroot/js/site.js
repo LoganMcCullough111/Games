39e95a7452a91f3bf4d852bfc2413c91a4f14b89
Logan McCullough <loganmccullough11@gmail.com>
2021-03-29 15:48:53 -0500

    Added code to cancel the default behavior of the <a> element

--- Games/wwwroot/js/site.js
+++ Games/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿
+﻿/* Function to start the sign in process */
+function vStartSignIn(eventData) {
+    //Cancel the default behavior for an <a> element.
+    eventData.preventDefault();
+    return;
+}
