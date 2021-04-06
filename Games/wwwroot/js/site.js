a4ccc665760a985d2d7739449f83eb61d04b3e35
Logan McCullough <loganmccullough11@gmail.com>
2021-04-06 15:48:35 -0500

    Made sign in look better and started create new account

--- Games/wwwroot/js/site.js
+++ Games/wwwroot/js/site.js
@@ -52,20 +52,119 @@ function eltCreateSignin() {
     let btnSignIn = document.createElement("button");
     btnSignIn.setAttribute("type", "button");
     btnSignIn.appendChild(txtSignIn);
+    let txtCancel = document.createTextNode("Cancel");
+    let btnCancel = document.createElement("button");
+    btnCancel.setAttribute("type", "button");
+    btnCancel.appendChild(txtCancel);
+    let divButtons = document.createElement("div");
+    divButtons.className = "CenterButtons";
+    divButtons.appendChild(btnSignIn);
+    divButtons.appendChild(btnCancel);
     let tdSIButton = document.createElement("td");
     tdSIButton.colSpan = "2";
-    tdSIButton.appendChild(btnSignIn);
+    tdSIButton.appendChild(divButtons);
+    let trSIButton = document.createElement("tr");
+    trSIButton.appendChild(tdSIButton);
+    //Fourth Row
+    let txtNoAccount = document.createTextNode("No account yet?");
+    let brLineBreak = document.createElement("br");
+    let txtClickHere = document.createTextNode("Click here");
+    let aClickHere = document.createElement("a");
+    aClickHere.setAttribute("href", "");
+    aClickHere.appendChild(txtClickHere);
+    let txtToCreateOne = document.createTextNode(" to create one!");
+    let tdCreateAccount = document.createElement("td");
+    tdCreateAccount.colSpan = "2";
+    tdCreateAccount.className = "SignIn";
+    tdCreateAccount.appendChild(txtNoAccount);
+    tdCreateAccount.appendChild(brLineBreak);
+    tdCreateAccount.appendChild(aClickHere);
+    tdCreateAccount.appendChild(txtToCreateOne);
+    let trCreateAccount = document.createElement("tr");
+    trCreateAccount.appendChild(tdCreateAccount);
+    //Create <tbody> element and put the rows in
+    let tbdyBody = document.createElement("tbody");
+    tbdyBody.appendChild(trUserName);
+    tbdyBody.appendChild(trPassword);
+    tbdyBody.appendChild(trSIButton);
+    tbdyBody.appendChild(trCreateAccount);
+    let tblSignin = document.createElement("table");
+    tblSignin.appendChild(tbdyBody);
+    //Put the table inside of the <section> element and the <section> element inside <div> element
+    let secSignin = document.createElement("section");
+    secSignin.className = "SignIn";
+    secSignin.appendChild(tblSignin);
+    //Sign in box built. Return it
+    return secSignin;
+}
+
+/*Function to create a box with text boxes for the username and password to create a new account. It returns a reference to a section
+ * element that contains the above.*/
+
+function eltCreateAccount() {
+    //Build tree from the bottom up
+    //First row of table
+    let txtUserName = document.createTextNode("User Name:");
+    let tdUserName = document.createElement("td");
+    tdUserName.appendChild(txtUserName);
+    let inpUserName = document.createElement("input");
+    inpUserName.setAttribute("type", "text");
+    let tdUNTextBox = document.createElement("td");
+    tdUNTextBox.appendChild(inpUserName);
+    let trUserName = document.createElement("tr");
+    trUserName.appendChild(tdUserName);
+    trUserName.appendChild(tdUNTextBox);
+    //Second Row
+    let txtPassword = document.createTextNode("Password:");
+    let tdPassword = document.createElement("td");
+    tdPassword.appendChild(txtPassword);
+    let inpPassword = document.createElement("input");
+    inpPassword.setAttribute("type", "password");
+    let tdPTextBox = document.createElement("td");
+    tdPTextBox.appendChild(inpPassword);
+    let trPassword = document.createElement("tr");
+    trPassword.appendChild(tdPassword);
+    trPassword.appendChild(tdPTextBox);
+    //Third Row
+    let txtRPassword = document.createTextNode("Re-Enter Password:");
+    let tdRPassword = document.createElement("td");
+    tdRPassword.appendChild(txtRPassword);
+    let inpRPassword = document.createElement("input");
+    inpRPassword.setAttribute("type", "password");
+    let tdRPTextBox = document.createElement("td");
+    tdRPTextBox.appendChild(inpRPassword);
+    let trRPassword = document.createElement("tr");
+    trRPassword.appendChild(tdRPassword);
+    trRPassword.appendChild(tdRPTextBox);
+    //Fourth row
+    let txtCreate = document.createTextNode("Create Account");
+    let btnCreate = document.createElement("button");
+    btnCreate.setAttribute("type", "button");
+    btnCreate.appendChild(txtCreate);
+    let txtCancel = document.createTextNode("Cancel");
+    let btnCancel = document.createElement("button");
+    btnCancel.setAttribute("type", "button");
+    btnCancel.appendChild(txtCancel);
+    let divButtons = document.createElement("div");
+    divButtons.className = "CenterButtons";
+    divButtons.appendChild(btnCreate);
+    divButtons.appendChild(btnCancel);
+    let tdSIButton = document.createElement("td");
+    tdSIButton.colSpan = "2";
+    tdSIButton.appendChild(divButtons);
     let trSIButton = document.createElement("tr");
     trSIButton.appendChild(tdSIButton);
     //Create <tbody> element and put the rows in
     let tbdyBody = document.createElement("tbody");
     tbdyBody.appendChild(trUserName);
     tbdyBody.appendChild(trPassword);
+    tbdyBody.appendChild(trRPassword);
     tbdyBody.appendChild(trSIButton);
     let tblSignin = document.createElement("table");
     tblSignin.appendChild(tbdyBody);
     //Put the table inside of the <section> element and the <section> element inside <div> element
     let secSignin = document.createElement("section");
+    secSignin.className = "SignIn";
     secSignin.appendChild(tblSignin);
     //Sign in box built. Return it
     return secSignin;
